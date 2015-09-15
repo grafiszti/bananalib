d236e95dc38d84df44cb7c884635066c478846c8
Matt <grafiszti@gmail.com>
2015-09-15 22:42:44 +0200

    started implementation of linkedList

---
+++ src/main/java/pl/grafiszti/bananalib/list/arrayList/LinkedList.java
@@ -0,0 +1,40 @@
+package pl.grafiszti.bananalib.list.arrayList;
+
+import pl.grafiszti.bananalib.list.List;
+
+/**
+ * Created by fisz on 15.09.15.
+ */
+public class LinkedList <E> implements List<E> {
+    public void add(E element) {
+        
+    }
+
+    public void removeFirst(E element) {
+
+    }
+
+    public void remove(E element) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(E element) {
+        return false;
+    }
+
+    public E get(int index) {
+        return null;
+    }
+
+    public int getIndex(E element) {
+        return 0;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/test/java/pl/grafiszti/bananalib/list/arrayList/LinkedListTest.java
@@ -0,0 +1,68 @@
+package pl.grafiszti.bananalib.list.arrayList;
+
+import pl.grafiszti.bananalib.list.ListTest;
+
+/**
+ * Created by fisz on 15.09.15.
+ */
+public class LinkedListTest implements ListTest {
+    public void init() {
+
+    }
+
+    public void testAddingIntegerElement() {
+
+    }
+
+    public void testAddingStringElement() {
+
+    }
+
+    public void testAdding100ElementsToList() {
+
+    }
+
+    public void testAddHugeAmountOfElements() {
+
+    }
+
+    public void testContainElementOnEmptyList() {
+
+    }
+
+    public void testContainElementOnNotEmptyList() {
+
+    }
+
+    public void testAddingAndRemovingOneElementOnEmptyList() {
+
+    }
+
+    public void testAddingAndRemovingOneElementOnNotEmptyList() {
+
+    }
+
+    public void testIsEmpty() {
+
+    }
+
+    public void testRemoveRepeatedElementsOnEmptyList() {
+
+    }
+
+    public void testRemoveRepeatedElementsOnNotEmptyList() {
+
+    }
+
+    public void testSize() {
+
+    }
+
+    public void testGetIndexOfElementOnEmptyList() {
+
+    }
+
+    public void testGetIndexOfElementOnNotEmptyList() {
+
+    }
+}
